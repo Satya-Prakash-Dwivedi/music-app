dcb1aeebe64e55c254e9b17ea558fef819ba0f9e
Satya-Prakash-Dwivedi <dwivediprakashsatya36@gmail.com>
2025-02-02 20:52:14 +0530

    Add code files for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
 import { main } from "motion/react-client"
 
 export default function Home() {
@@ -6,6 +12,12 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       {/* <h1 className="text-2xl text-center">5atya 5erver</h1> */}
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
+      <UpcomingWebinars />
+      <Instructors /> 
+      <Footer />
     </main>
     
   );
